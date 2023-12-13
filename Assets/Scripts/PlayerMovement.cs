d44133579372c834cc233ebd373fb1092a148ce9
ZeRcooI <mihanpan1996@mail.ru>
2023-12-13 07:43:52 +0300

    stageThree

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _runSpeed = 10f;
     [SerializeField] private float _jumpSpeed = 5f;
+    [SerializeField] private float _climbSpeed = 5f;
+    private float _gravityScaleAtStart;
 
     private Vector2 _moveInput;
     private Rigidbody2D _rigidbody2D;
@@ -22,7 +24,10 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnJump(InputValue inputValue)
     {
-        if (!_capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; } //todo узнать за логику
+        if (!_capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        } //todo узнать за логику
 
         if (inputValue.isPressed)
         {
@@ -32,15 +37,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
-        _animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+
+        _gravityScaleAtStart = _rigidbody2D.gravityScale;
     }
 
     private void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
+    }
+
+    private void ClimbLadder()
+    {
+        if (!_capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            _rigidbody2D.gravityScale = _gravityScaleAtStart;
+            return;
+        } //todo узнать за логику
+
+        Vector2 climbVelocity = new Vector2(_rigidbody2D.velocity.x, _moveInput.y * _climbSpeed);
+        _rigidbody2D.velocity = climbVelocity;
     }
 
     private void FlipSprite()
@@ -55,11 +75,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void Run()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
         Vector2 playerVelocity = new Vector2(_moveInput.x * _runSpeed, _rigidbody2D.velocity.y);
-
         _rigidbody2D.velocity = playerVelocity;
 
-        _animator.SetBool("isRunning", playerHasHorizontalSpeed);
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
+        _animator.SetBool("IsRunning", playerHasHorizontalSpeed);
     }
 }
